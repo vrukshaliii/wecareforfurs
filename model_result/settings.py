c717cb4f0dbca03de42a98d1b127745acae98ba6
Vrukshali-26 <tvrukshali@gmail.com>
2023-03-09 11:10:23 +0530

    Created dockerfile

--- model_result/settings.py
+++ model_result/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a!mxbxz4*cbc3q(ww5di+*^ns@pa(1*1y_^)u_$_y4kwyxb%n=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
