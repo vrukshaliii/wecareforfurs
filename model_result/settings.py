83aeaf78223f3b7199b3825ade9172f521d289c4
Vrukshali-26 <tvrukshali@gmail.com>
2023-03-13 10:52:01 +0530

    changed some lines in settings

--- model_result/settings.py
+++ model_result/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a!mxbxz4*cbc3q(ww5di+*^ns@pa(1*1y_^)u_$_y4kwyxb%n='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
